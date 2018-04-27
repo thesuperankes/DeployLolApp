da9a8d7d5ecf922d29dca68a4f516a78fd4c999d
Andres Lopez <andres.lopez@banlinea.com>
2018-04-27 10:46:38 -0500

    DeployApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+
+import { app_routing } from './app.routes';
+
+import { SummonerService } from './services/summoner.service';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MatchComponent } from './components/match/match.component';
+import { ChampionsComponent } from './components/champions/champions.component';
+import { SummonerComponent } from './components/summoner/summoner.component';
+import { SummonerSearchComponent } from './components/summoner-search/summoner-search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MatchComponent,
+    ChampionsComponent,
+    SummonerComponent,
+    SummonerSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    SummonerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
